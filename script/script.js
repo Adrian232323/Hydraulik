777d5c2226fe36b8b9dc21fe4ac498be22a28602
Adrian232323 <133059674+Adrian232323@users.noreply.github.com>
2024-06-07 12:36:36 +0200

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,8 @@
+const faqs = document.querySelectorAll('.faq-box')
+
+
+faqs.forEach((faq) => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('faq-answer-active');
+    })
+})
